cdb96bef172e26fa93b7a49b4aa43451197e47d2
riaru <ori-riaru@proton.me>
2025-03-06 14:21:20 -0500

    Added flake for Nixos

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (
+            python3.withPackages (python-pkgs:
+              with python-pkgs; [
+                ruff
+                flask-cors
+                flask
+              ])
+          )
+        ];
+
+        shellHook = ''
+
+        '';
+      };
+    });
+}
