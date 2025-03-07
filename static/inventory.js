6d8c873218b70cddbc2ab829c8b8934d54530a29
nabihaaaa <nabiha.shah@ontariotechu.net>
2025-03-07 14:16:25 -0500

    fix edit button

--- static/inventory.js
+++ static/inventory.js
@@ -281,7 +281,7 @@ function updateTable() {
                 <td>${item.color}</td>
                 <td>
                     <button class="edit-button" onclick="openEditPopup(${item.id})">
-                        <img style="width: 1rem" src="./icons/edit.svg">
+                        <img style="width: 1rem" src="/static/icons/edit.svg"">
                     </button>
                 </td>
             `;
