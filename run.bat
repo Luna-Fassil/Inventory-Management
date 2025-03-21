9e580cc7aa1ae9baf5c74fa2ddd79b03529a777b
nabihaaaa <nabiha.shah@ontariotechu.net>
2025-03-21 10:01:24 -0400

    added clean to build script

--- run.bat
+++ run.bat
@@ -7,11 +7,12 @@ echo ==========================================
 echo Activating virtual environment...
 call venv\Scripts\activate
 
+
 :: Step 2: Start the Flask application
 echo Starting Flask server...
 python inventory.py
 
-:: Keep the terminal open to display logs
+:: Keep the terminal open
 echo ==========================================
 echo Server is running! Open http://127.0.0.1:5000/ in your browser.
 echo Press CTRL+C to stop the server.
